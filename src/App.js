03ca1b389789192004657c52d1409c85cbea8804
chenyuf2 <chenyuf2@illinois.edu>
2024-03-15 00:52:52 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import { useRef } from "react";
+import Scene from "components/Scene/Scene";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 function App() {
+  const sceneRef = useRef(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [0, 0, 70], fov: 45 }}>
+      <Scene ref={sceneRef} />
+      {/* <OrbitControls /> */}
+    </Canvas>
   );
 }
 
